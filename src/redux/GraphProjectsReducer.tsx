3d3b26b29ad8862811ebbbc96b85924df199103a
reinisvla <reinisvla@gmail.com>
2022-02-11 12:12:52 +0200

    subscribed reducers in store

--- src/redux/GraphProjectsReducer.tsx
+++ src/redux/GraphProjectsReducer.tsx
@@ -21,4 +21,5 @@ export const graphProjectsReducer = (
             }
         }
     }
+    return state;
 }
--- src/redux/Store.tsx
+++ src/redux/Store.tsx
@@ -1,13 +1,17 @@
 import { combineReducers, createStore, Store } from "redux";
 import { IProjectState } from "./Projects";
 import { projectsReducer } from "./ProjectsReducer";
+import { IGraphProjectState } from './GraphProjects'
+import { graphProjectsReducer } from './GraphProjectsReducer';
 
 export interface IAppState {
     readonly projects: IProjectState;
+    readonly graphProjects: IGraphProjectState;
 };
 
 const rootReducer = combineReducers<IAppState>({
     projects: projectsReducer,
+    graphProjects: graphProjectsReducer
 });
 
 export function configureStore() : Store<IAppState> {
